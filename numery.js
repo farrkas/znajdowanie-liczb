755b2942e7a1e2c27e8faf66093c9764e6814c5d
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-07 23:28:20 +0100

    pierwszy

---
+++ numery.js
@@ -0,0 +1,14 @@
+function numery(ciag){
+
+//if (ciag.length>0){
+    
+    var tab=[...ciag];
+    var tabNum=[];
+    while (i<(tab.length-1)){
+        isNan.ciag[i]=true;
+        tabNum.push(tab[i]);
+    }
+
+}
+
+numery("125dsa4812");
